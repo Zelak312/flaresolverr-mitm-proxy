c3858e28cd66e0c52bfe796780a609045694249b
Zelak312 <zelak312@gmail.com>
2026-02-03 11:26:39 -0500

    chore(release): prepare for v1.1.0

--- release.sh
+++ release.sh
@@ -10,18 +10,9 @@ echo "Preparing $1..."
 # update the version
 msg="# managed by release.sh"
 # update the changelog
-npx git-cliff --config cliff.toml --tag "$1" 421d125..HEAD > CHANGELOG.md
 git add -A && git commit -m "chore(release): prepare for $1"
 git show
 # generate a changelog for the tag message
-export GIT_CLIFF_TEMPLATE="\
-	{% for group, commits in commits | group_by(attribute=\"group\") %}
-	{{ group | upper_first }}\
-	{% for commit in commits %}
-		- {% if commit.breaking %}(breaking) {% endif %}{{ commit.message | upper_first }} ({{ commit.id | truncate(length=7, end=\"\") }})\
-	{% endfor %}
-	{% endfor %}"
-changelog=$(npx git-cliff --config cliff.toml --unreleased --strip all)
-git tag -a "$1" -m "Release $1" -m "$changelog"
+git tag -a "$1" -m "Release $1"
 echo "Done!"
 echo "Now push the commit (git push) and the tag (git push --tags)."
